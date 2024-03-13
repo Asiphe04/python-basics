7f66fc6cf94d37326a93bb46121bc8b994a4c67a
Asiphe04 <asiphendimlana40@gmail.com>
2024-03-13 12:09:40 +0200

    string basics

--- index.py
+++ index.py
@@ -1,1 +1,43 @@
-print('Hello World !') #Prinitng out a statemtent, or outputt
+# Prinitng out a statemtent, or output
+
+print('Hello World !') 
+
+#--------------------------------------------------------------------------------------------------
+
+#Variables
+
+doctor = 'DR. Asiphe' #String
+
+doc_number = 781149972 #number
+
+patient_sick = True #booean
+
+
+
+print('Hello' + ' ' + doctor )
+
+#IntToStr
+print('Hello' + ' ' + doctor )
+
+#--------------------------------------------------------------------------------------------------
+
+#cconverting text to uppercase, lowercase and all upercases 
+
+dummyText = 'change to uppercase'
+
+print(dummyText.capitalize()) #captilizing first letter
+print(dummyText.upper()) #captailiing all letters
+print(dummyText.lower()) #lowering all letters
+
+#--------------------------------------------------------------------------------------------------
+
+
+print(len(dummyText)) #checking length of a string
+
+print(dummyText[3]) #targetting a specific position of a letter
+
+print(dummyText.index('n')) #getting the position  where the specific letter is 
+
+print(dummyText.replace('Change','Convert')) #replacing a word 
+
+
